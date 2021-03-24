094b9d89958d74c0e8d0ed12495b2ae957ae4727
vaishpy <ravichandran.vaishu@gmail.com>
2021-03-24 14:02:34 -0500

    gh pages

--- src/categoryPicker.js
+++ src/categoryPicker.js
@@ -5,17 +5,20 @@ function CategoryPicker(props){
     const color ={color:"white"}
 
     function handleClick(event){ console.log(event.target.nodeName)
-        if(event.target.nodeName !== "H1"){
-
+        if(event.target.nodeName === "DIV" ){
+            if(event.target.id !== "mainDiv"){
             props.chooseCategory(event.target.textContent)
+            }
            
+        }else{
+            event.preventDefault()
         }
 
     }
 
 
     return(
-    <div className="category-picker" onClick={handleClick}>
+    <div className="category-picker" onClick={handleClick} id="mainDiv">
         <h1 style={color}>Pick A Category</h1>
         
         <div className="category">Science And Nature</div>
